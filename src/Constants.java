1b044b5d7c856eff9ce6570a14bdce6fdb805dc7
Ritesh Kumar <ritesh.kumar.ece13@iitbhu.ac.in>
2016-07-10 22:49:36 +0530

    constants separated

---
+++ src/Constants.java
@@ -0,0 +1,15 @@
+
+public class Constants {
+
+	static final int SERVER_PORT = 8993;
+
+	// String used for communicating with the server
+	static final String CAMERA_SWAP = "0110";
+	static final String CAM_PRIMARY_SELECT = "0";
+	static final String CAM_SECONDARY_SELECT = "1";
+
+	static final String LEFT_DOWN_ACTION = "10";
+	static final String LEFT_UP_ACTION = "00";
+
+	static final String RIGHT_CLICK_ACTION = "0X";
+}
--- src/RemoteDroidServer.java
+++ src/RemoteDroidServer.java
@@ -23,16 +23,15 @@ public class RemoteDroidServer {
 	private static String line;
 	private static boolean isConnected = true;
 	private static Robot robot;
-	private static final int SERVER_PORT = 8993;
+	private static boolean isSecondaryCamActive =false;
 
 	public static void main(String[] args) {
-		boolean leftpressed = false;
-		boolean rightpressed = false;
 
 		try {
 			robot = new Robot();
-			server = new ServerSocket(SERVER_PORT); // Create a server socket on
-													// port 8998
+			server = new ServerSocket(Constants.SERVER_PORT); // Create a server
+																// socket on
+			// port 8993
 			client = server.accept(); // Listens for a connection to be made to
 										// this socket and accepts it
 			in = new BufferedReader(new InputStreamReader(client.getInputStream())); // the
@@ -51,44 +50,53 @@ public class RemoteDroidServer {
 			System.out.println("Error in creating robot instance");
 			System.exit(-1);
 		}
-		int x = 0, y = 0, i;
+		int x = 0, y = 0;
 		// read input from client while it is connected
 		while (isConnected) {
 			try {
 
 				line = in.readLine();
 				// System.out.println(line);//read input from client
-				if (line.equals("left_down")) {
+				if (line.equals(Constants.LEFT_DOWN_ACTION)) {
 					robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-				} else if (line.equals("left_up")) {
+				} else if (line.equals(Constants.LEFT_UP_ACTION)) {
 					robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
 
-				} else if (line.equals("right")) {
+				} else if (line.equals(Constants.RIGHT_CLICK_ACTION)) {
 					robot.mousePress(InputEvent.BUTTON3_MASK);
 					robot.mouseRelease(InputEvent.BUTTON3_MASK);
-				} else {
-					 System.out.println(line); //print whatever we get from
+				} else if(line.equals(Constants.CAMERA_SWAP)){
+					isSecondaryCamActive=true;
+				}else {
+					System.out.println(line); // print whatever we get from
 					// client
 					String[] cordinates = line.split(" ");
 					int dx = Integer.parseInt(cordinates[0]);
 					int dy = Integer.parseInt(cordinates[1]);
 					Point p = MouseInfo.getPointerInfo().getLocation();
-					if(dx>-5&dx<0)
-						dx=0;
-					if(dy>-5&dy<0)
-						dy=0;
+					if (dx > -5 & dx < 0)
+						dx = 0;
+					if (dy > -5 & dy < 0)
+						dy = 0;
 					x = p.x;
 					y = p.y;
 					// MouseUtils.mouseGlide(x, y, dx, dy, 100, 100);
-					x=x+dy;
-					y=y-dx;
-					robot.mouseMove(x, y);
-					/*for (i = 0; i < 10; i++) {
-						x = x + dy / 5;
-						y = y - dx / 5;
-						robot.mouseMove(x, y);
-						robot.delay(1);
-					}*/
+					if(!isSecondaryCamActive){
+						//when primary camera is active
+						x = x + dy;
+						y = y - dx;
+					}else{
+						//when secondary camera is active
+						//TODO change code to adapt to secondary cam
+						x = x + dy;
+						y = y - dx;
+					}
+					
+//					robot.mouseMove(x, y);
+					/*
+					 * for (i = 0; i < 10; i++) { x = x + dy / 5; y = y - dx /
+					 * 5; robot.mouseMove(x, y); robot.delay(1); }
+					 */
 					// robot.mouseMove(x+2*dy, y-2*dx);
 					if (x < 0)
 						x = 0;
@@ -99,44 +107,11 @@ public class RemoteDroidServer {
 						y = 0;
 					else if (y > 768)
 						y = 768;
-
+					robot.mouseMove(x, y);
 					// System.out.println(x);
 					// System.out.println(y);
 				}
 
-				/*
-				 * //if user clicks on next if(line.equalsIgnoreCase("next")){
-				 * //Simulate press and release of key 'n'
-				 * robot.keyPress(KeyEvent.VK_N);
-				 * robot.keyRelease(KeyEvent.VK_N); } //if user clicks on
-				 * previous else if(line.equalsIgnoreCase("previous")){
-				 * //Simulate press and release of key 'p'
-				 * robot.keyPress(KeyEvent.VK_P);
-				 * robot.keyRelease(KeyEvent.VK_P); } //if user clicks on
-				 * play/pause else if(line.equalsIgnoreCase("play")){ //Simulate
-				 * press and release of spacebar
-				 * robot.keyPress(KeyEvent.VK_SPACE);
-				 * robot.keyRelease(KeyEvent.VK_SPACE); } //input will come in
-				 * x,y format if user moves mouse on mousepad else
-				 * if(line.contains(",")){ float
-				 * movex=Float.parseFloat(line.split(",")[0]);//extract movement
-				 * in x direction float
-				 * movey=Float.parseFloat(line.split(",")[1]);//extract movement
-				 * in y direction Point point =
-				 * MouseInfo.getPointerInfo().getLocation(); //Get current mouse
-				 * position float nowx=point.x; float nowy=point.y;
-				 * robot.mouseMove((int)(nowx+movex),(int)(nowy+movey));//Move
-				 * mouse pointer to new location } //if user taps on mousepad to
-				 * simulate a left click else if(line.contains("left_click")){
-				 * //Simulate press and release of mouse button 1(makes sure
-				 * correct button is pressed based on user's dexterity)
-				 * robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-				 * robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK); } //Exit if
-				 * user ends the connection else
-				 * if(line.equalsIgnoreCase("exit")){ isConnected=false; //Close
-				 * server and client socket // server.close(); //
-				 * client.close(); }
-				 */
 			} catch (IOException e) {
 				System.out.println("Read failed");
 				System.exit(-1);
